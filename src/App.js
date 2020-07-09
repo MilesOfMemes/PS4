eadc1e28f81a47e8660475042bad8d181bf4d028
Miles Mason <mmason02@g.uafs.edu>
2020-07-09 17:04:33 -0500

    problem set 4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    books : [],
+    text : "",
+  }
+  
+  // searchHandle = () => {
+  //   axios
+  //   .get("https://www.googleapis.com/books/v1/volumes?q=cat&key=AIzaSyBjvSSu8pvnQiipnJLflOpQs5HQlplPUKQ&maxResults=3")
+  //   .then((data) => {
+  //     console.log(data.data.items);
+  //     this.setState({
+  //       books: data.data.items,
+  //     })
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   })
+  // }
+
+  getInputHandle = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
+  searchHandle = () => {
+    axios
+    .get("https://www.googleapis.com/books/v1/volumes?q="+this.state.text+"&key=AIzaSyBjvSSu8pvnQiipnJLflOpQs5HQlplPUKQ&maxResults=8")
+    .then((data) => {
+      console.log(this.state.text);
+      console.log(data.data.items);
+      this.setState({
+        books: data.data.items,
+      })
+    })
+    .catch((err) => {
+      console.log(err);
+    });  
+  }
+
+handleDescription = (book) => {
+  console.log(book.volumeInfo.description);
+  document.getElementById("description").innerHTML = book.volumeInfo.description;
+
+}
+
+
+  render(){
+    return (
+      <div className="App">
+        <input type="text" onChange={(e) => this.getInputHandle(e)}></input>
+        <button onClick={this.searchHandle}>Search</button>
+        {this.state.books.map((book) => {
+          return(
+            <div>
+              {console.log(book.volumeInfo.imageLinks.thumbnail)}
+              
+              <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title} onClick={(e) => this.handleDescription(book,e)}/>
+              
+              {<div>{book.volumeInfo.title}</div>}
+              {" Author: "} 
+              {book.volumeInfo.authors}
+              
+              <div id="description">
+
+              </div>
+            </div >
+            
+          ); 
+      })}
+
+      </div>
+    );
+  }
 }
 
 export default App;
